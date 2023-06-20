81d12281a99cfc9d24bf4b48c43d4f55846156d5
Zachary Proser <zackproser@gmail.com>
2023-06-20 08:49:24 -0400

    Implement simple HTTPS server

---
+++ ssl_simple_server/README.md
@@ -0,0 +1,9 @@
+# Usage
+
+To run this code, first use OpenSSL to create a new self-signed certificate:
+
+```
+openssl req -new -x509 -keyout cert.pem -out cert.pem -days 365 -nodes 
+```
+
+
---
+++ ssl_simple_server/server.py
@@ -0,0 +1,15 @@
+import http.server
+import socketserver
+import ssl
+
+context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+context.load_cert_chain("cert.pem")
+
+server_address = ("localhost", 4443)
+
+handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(server_address, handler) as httpd:
+    httpd.socket = context.wrap_socket(httpd.socket, server_side=True)
+
+    httpd.serve_forever()
